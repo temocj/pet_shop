24f903701034967a77ab97720f5f391576a8367e
temocj <cuauhtemoc.jacobson@gmail.com>
2020-09-03 14:25:18 +0100

    added find_pet_by_name()

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -24,3 +24,9 @@ def get_pets_by_breed(pet_shop, breed):
         if pet["breed"] == breed:
             found_pets.append(pet)
     return found_pets
+
+def find_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            return pet
+    return None
