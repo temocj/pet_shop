68cae393ef33d4f67b1355f8c7769e7fa99b67be
temocj <cuauhtemoc.jacobson@gmail.com>
2020-09-03 14:31:43 +0100

    add get_customer_cash()

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -38,3 +38,6 @@ def remove_pet_by_name(pet_shop, pet_name):
 
 def add_pet_to_stock(pet_shop, new_pet):
     pet_shop["pets"].append(new_pet)
+
+def get_customer_cash(customer):
+    return customer["cash"]
