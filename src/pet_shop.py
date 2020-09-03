5aa1522131f0c423db6c28dbb2af81a167db96a8
temocj <cuauhtemoc.jacobson@gmail.com>
2020-09-03 14:14:07 +0100

    add add_or_remove_cash()

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -5,3 +5,6 @@ def get_pet_shop_name(pet_shop):
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, amount):
+    pet_shop["admin"]["total_cash"] += amount
