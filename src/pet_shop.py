a65501ec5ed8c7229d797bf95a09eacd77bc9353
temocj <cuauhtemoc.jacobson@gmail.com>
2020-09-03 14:58:04 +0100

    add sell_pet_to_customer()

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -53,3 +53,12 @@ def add_pet_to_customer(customer, pet):
 
 def customer_can_afford_pet(customer, pet):
     return customer["cash"] >= pet["price"]
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if pet != None:
+        if customer_can_afford_pet(customer, pet):
+            remove_customer_cash(customer, pet["price"])
+            remove_pet_by_name(pet_shop, pet["name"])
+            add_pet_to_customer(customer, pet)
+            increase_pets_sold(pet_shop, 1)
+            add_or_remove_cash(pet_shop, pet["price"])
